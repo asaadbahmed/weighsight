97c035be07a9424d20e158645b661cd215b8d595
Asaad Ahmed <asaadbinahmed@gmail.com>
2025-05-04 15:24:07 -0400

    touch ups

--- app.py
+++ app.py
@@ -1,35 +1,41 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
 
 st.title("Weighsight")
 dates = ["2025-01-01", "2025-01-08", "2025-01-15", "2025-01-22"] # populate this via Google Calendar
 weights = ["120 lbs", "125 lbs", "120 lbs", "135 lbs"] # populate this via Google Calendar
 data = pd.DataFrame({
-    "date": pd.to_datetime(dates),
+    "date": dates,
     "weight": weights,
     "week": [f"Week {i+1}" for i in range(len(dates))]
 })
 
-fig = px.line(
-    data,
-    x="week",
-    y="weight",
-    title="2025 Weight Graph",
-    markers=True,
-    line_shape="spline"
+trace = go.Scatter(
+    x=data["date"],
+    y=data["weight"],
+    mode="lines+markers",
+    line=dict(color="#00ff00", width=3, shape="spline"),
+    marker=dict(color="#00ff00", size=6),
+    fill="tozeroy",
+    fillcolor="rgba(30, 215, 96, 0.15)",
+    hovertemplate="<b>Date:</b> %{x|%b %d, %Y}<br><b>Weight:</b> %{y}<extra></extra>"
 )
 
-fig.update_traces(line=dict(color="#00ff00", width=3), marker=dict(color="#00ff00", size=5), 
-    hovertemplate="<b>Date:</b> %{customdata|%b %d, %Y}<br><b>Weight:</b> %{y}<extra></extra>",
-    customdata=data["date"])
+fig = go.Figure(data=[trace])
 fig.update_layout(
     template="plotly_white",
-    title_font_size=22,
-    xaxis_title="Week",
-    yaxis_title="Weight (lbs)",
-    font=dict(family="Helvetica", size=14),
-    hovermode="x unified"
+    plot_bgcolor="#191414",
+    paper_bgcolor="#121212",
+    title="2025 Weight Graph",
+    title_font_size=24,
+    font=dict(family="Helvetica", size=14, color="white"),
+    xaxis_title="<b>Date</b>",
+    yaxis_title="<b>Weight (lbs)</b>",
+    xaxis=dict(showspikes=False, color="white", showgrid=False),
+    yaxis=dict(showspikes=False, gridcolor="#333", color="white", showgrid=False),
+    hovermode="x unified",
+    margin=dict(t=60, b=40, l=60, r=40)
 )
 
 st.button("Connect your Google account", on_click=st.login)
